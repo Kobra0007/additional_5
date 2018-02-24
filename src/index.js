c8fb48f722de4195c3d063aef73be8a941b46582
Kobra0007 <kobra007@github.com>
2018-02-25 02:50:48 +0300

    Solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,26 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+
+    var objBrackets = [];
+    for (var i = 0; i < str.length; i++) {
+
+        for (var j = 0; j < bracketsConfig.length; j++) {
+            if (str[i] == bracketsConfig[j][0]) {
+                objBrackets.push(str[i]);
+                //str = str.replace(bracketsConfig[j][0],'-');
+
+            }
+            if (str[i] == bracketsConfig[j][1]) {
+                if (objBrackets[objBrackets.length - 1] == bracketsConfig[j][0]) {
+                    objBrackets.pop();
+                    //	str = str.replace(bracketsConfig[j][1],'+');
+                }
+
+            }
+        }
+    }
+    if (objBrackets.length) {
+        return false;
+    }
+    
+    return true;
 }
